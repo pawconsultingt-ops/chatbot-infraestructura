8dd24772092730df70711747a0500749e31b3c28
pawconsultingt-ops <pawconsultingt@gmail.com>
2026-04-30 23:13:56 -0500

    Modificado conexion backend

--- frontend/app.js
+++ frontend/app.js
@@ -38,8 +38,8 @@ const auth        = getAuth(firebaseApp);
 // ---------------------------------------------------------------------------
 
 //const API_URL = "http://localhost:8001";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8001";
-
+//const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8001";
+const API_URL = "https://chatbot-infraestructura-production.up.railway.app";
 
 // ---------------------------------------------------------------------------
 // DOM refs
